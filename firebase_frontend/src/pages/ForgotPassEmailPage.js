323360e8150800469fb5449e9779ee21635c2b22
Rachel Leiner <userrachellee@gmail.com>
2024-07-30 06:09:30 -0400

    formatting

--- firebase_frontend/src/pages/ForgotPassEmailPage.js
+++ firebase_frontend/src/pages/ForgotPassEmailPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import ChangePassword from '../components/ForgotPassEmail';
+import ForgotPassEmail from '../components/ForgotPassEmail';
 import '../styles/ChangePassword.css';
 
 const ForgotPasswordPage = () => {
