a2509fca8053b99e51c40f33cb989b24b9bcc8e5
jadtfen <119529173+jadtfen@users.noreply.github.com>
2024-07-30 05:33:01 -0400

    Create InviteSentPage.js

---
+++ firebase_frontend/src/pages/InviteSentPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import InviteSent from '../components/InviteSent'; 
+import '../styles/InviteSentPage.css';
+
+const InviteSentPage = () => {
+  return (
+    <div className="container">
+      <InviteSent />
+    </div>
+  );
+};
+
+export default InviteSentPage;
